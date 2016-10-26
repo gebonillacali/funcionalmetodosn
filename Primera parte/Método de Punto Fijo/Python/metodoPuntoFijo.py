af83b56c1e059988c03b5e0e363d07217a0b6e1b
Gustavo E Bonilla <gbonilla@unbosque.edu.co>
2016-10-26 11:28:26 -0500

    Areglando metodo punto fijo python, agregando porcentaje

--- Primera parte/Método de Punto Fijo/Python/metodoPuntoFijo.py
+++ Primera parte/Método de Punto Fijo/Python/metodoPuntoFijo.py
@@ -4,13 +4,18 @@ def metodoPuntoFijo( x0, e1, e2, yi ):
    x = testFuncion(x0, yi);
    salida.append(x);
    
-   while (abs(x-x0) > e1):
-    x0 = x
-    x = testFuncion(x0,yi);
-    salida.append(x);
+   while (True):
+   	if (testFuncion(x, yi) < (e2/100)):
+   		break;
+   	if (abs(x-x0) < (e1/100)):
+   		break;
+   	x0 = x;
+   	x = testFuncion(x0,yi);
+   	salida.append(x);
    return salida;
 
 def testFuncion(x, yi):
    return (x**3 - yi) / 4;
 
-print(metodoPuntoFijo(1, 0.0005,0.0005, 0));
+print(metodoPuntoFijo(0.05, 0.005,0.005, 0));
+
