6e2ca1346a1b24ead10a2bce19125f124c7a8b11
Gustavo E Bonilla <gbonilla@unbosque.edu.co>
2016-10-26 11:18:34 -0500

    Arreglo al método de la falsa posicion en C

--- Primera parte/Método de la Falsa Posición/C/metodofalsaposicion.c
+++ Primera parte/Método de la Falsa Posición/C/metodofalsaposicion.c
@@ -1,69 +1,47 @@
 #include <stdio.h>
 #include <math.h>
 
-int getArraySize(float *array) {
-    return sizeof(array) / sizeof(array);
-}
-
-
-float * addItemToArray(float item, float *array) {
-    float *newArray;
-    
-    int tamanoActual = getArraySize(array);
-    printf("TamAct:%d \r\n",tamanoActual);
-    if (tamanoActual == 1) {
-        array[0] = item;
-        return array;
-    } else {
-        array = malloc(sizeof(float) * (tamanoActual + 1));
-        int i = 0;
-        for (i = 0; i < tamanoActual; i++) {
-            array[i] = array[i];
-        }
-        array[tamanoActual] = item;
-        return newArray;
-    }
-}
-
-
-int checkError(float xActual, float xAnterior, float error) {
-    return fabs((xActual - xAnterior)/xActual) * 100 < error;
-}
+int index = 0;
 
 float testFuncion(float x, float yi) {
     return (x*x*x) - (4 *x) - yi;
 }
 
 float * metodoFalsaPosicion(float a, float b, float e1, float e2, float yi) {
-    float xActual = a;
-    float *salida = malloc(sizeof(float) * 1);
-    do {
-        xActual = b - ((testFuncion(b, yi) * (a-b))/(testFuncion(a, yi) - testFuncion(b, yi)));
-        if ((testFuncion(a, yi) * testFuncion(xActual, yi)) > 0) {
-            a = xActual;
+    
+    static float salida[10000]; 
+    
+    while (1) {
+		
+		float c = b - ((testFuncion(b, yi) * (b-a)) / (testFuncion(b, yi) - testFuncion(a, yi)));
+		salida[index] = c;
+		index++;
+		
+		if (fabs(testFuncion(c, yi)) < (e2/100)) {
+			break;
+		}
+		
+		if (fabs(b - a) < (e1/100)) {
+			break;
+		}
+        
+		
+        if ((testFuncion(a, yi) * testFuncion(c, yi)) < 0) {
+            b = c;
         } else {
-            if((testFuncion(a, yi) * testFuncion(xActual, yi)) < 0) {
-                b = xActual;
-            } 
-            else {
-                if((testFuncion(a, yi) * testFuncion(xActual, yi)) == 0)
-                    break;
-            }
+            a = c;
         }
-        salida = addItemToArray(xActual, salida);
-    }
-    while (!checkError(xActual, a, e1) && testFuncion(xActual, yi) < e2);
+        
+    }    
     return salida;
 }
 
 
 int main () {
-    float * salida = metodoFalsaPosicion(1, 4, 0.0005,0.0005, 0);
-    int n = getArraySize(salida);
+    float *salida = metodoFalsaPosicion(1, 10, 0.5,0.5, 0);
+    int n = index;
     int i = 0;
     for (i = 0; i < n ; i++) {
         printf("%f\r\n", salida[i]);
     }
 }
-
-
