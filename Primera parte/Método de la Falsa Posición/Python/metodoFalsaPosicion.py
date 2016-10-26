c0d855de63d1a8cf22b0688771210b755e242516
Gustavo E Bonilla <gbonilla@unbosque.edu.co>
2016-10-26 11:20:02 -0500

    Arreglo al método de la falsa posición en Python

--- Primera parte/Método de la Falsa Posición/Python/metodoFalsaPosicion.py
+++ Primera parte/Método de la Falsa Posición/Python/metodoFalsaPosicion.py
@@ -5,17 +5,17 @@ def metodoFalsaPosicion(a,b, e1, e2, yi ):
    	c = b - ((testFuncion(b,yi)*(b-a)) / (testFuncion(b,yi) - testFuncion(a,yi)));
    	salida.append(c);
    	print(c);
-   	if (abs(testFuncion(c,yi)) < e2):
+   	if (abs(testFuncion(c,yi)) < (e2/100)):
    		break;
-   	if (abs(b-a) < e1):
+   	if (abs(b-a) < (e1/100)):
    		break;
-   	if ((testFuncion(c,yi) * testFuncion(b,yi)) > 0):
+   	if ((testFuncion(c,yi) * testFuncion(a,yi)) < 0):
+   		b = c;
+   	else:
    		a = c;
-   	if ((testFuncion(c,yi) * testFuncion(a,yi)) > 0):
-   		b = c
    return salida;
 
 def testFuncion(x, yi):
    return x**3 - (4*x) - yi;
 
-print(metodoFalsaPosicion(0.5, 4, 0.005,0.005, 0));
+print(metodoFalsaPosicion(0.5, 10, 0.5,0.5, 0));
