a994e5c31968d40c73365ba63da29e9ece412eea
Gustavo E Bonilla <gbonilla@unbosque.edu.co>
2016-10-27 23:28:31 -0500

    Cambiando comment

--- Primera parte/Método de la Falsa Posición/Haskell/metodofalsaposicion.hs
+++ Primera parte/Método de la Falsa Posición/Haskell/metodofalsaposicion.hs
@@ -1,4 +1,4 @@
---Funcion método Secante
+--Funcion método Falsa Posición
 metodoFalsaPosicion :: (Ord a, Num a, Show a, Fractional a) => a -> a -> a -> a -> a -> [a]
 metodoFalsaPosicion a b e1 e2 yi = if (abs (testFuncion c yi)) < (e2/100) || (abs (b-a)) < (e1/100) then c:[] else if ((testFuncion c yi) * (testFuncion a yi)) < 0 then c:[] ++ (metodoFalsaPosicion a c e1 e2 yi) else c:[] ++ (metodoFalsaPosicion c b e1 e2 yi) where c = b - (((testFuncion b yi) * (b-a)) / ((testFuncion b yi) - (testFuncion a yi)))
 
