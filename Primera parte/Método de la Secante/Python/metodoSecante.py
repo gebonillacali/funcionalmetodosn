550a97749e53643d65a4598a0bd9bb59c09694aa
Gustavo E Bonilla <gbonilla@unbosque.edu.co>
2016-10-27 23:34:59 -0500

    Solucion Metodo de la Secante en Python

---
+++ Primera parte/Método de la Secante/Python/metodoSecante.py
@@ -0,0 +1,22 @@
+def metodoSecante( x0, e1, e2, yi ):
+   "Implementa el método de la Secante"
+   salida = [];
+   while (True):
+      x1 = x0 - (testFuncion (x0, yi) / testFuncionPrima(x0));
+      x = x1 - (((x1-x0)/(testFuncion(x1,yi)-testFuncion(x0,yi))) * testFuncion(x1,yi));
+      salida.append(x);
+      if (abs(testFuncion(x,yi))<(e2/100)):
+         break;
+      if (abs(x-x0)<(e1/100)):
+         break;
+      x0 = x1;
+      x1 = x;
+   return salida;
+
+def testFuncion(x, yi):
+   return x**3 - (4*x) - yi;
+
+def testFuncionPrima(x):
+   return 3*(x**2) - 4;
+
+print (metodoSecante(100,0.05,0.05,0))
